7d7ed6ddb7c5c9dad3dcb4fb5cf70f73a3d8faac
PilJoong, in CLO Office <43630935+piljoong-jeong@users.noreply.github.com>
2022-06-04 21:17:33 +0900

    [ENV] OpenCV CPU build test
    TODO: build with CUDA: Nvidia driver, cmake CUDA flags etc

--- run_docker.sh
+++ run_docker.sh
@@ -2,8 +2,11 @@ NAME_PROJECT=$(basename ${PWD})
 
 COMMANDS_BUILD="\
 	-t $NAME_PROJECT -f docker/Dockerfile ."
-docker run $COMMANDS_RUN
+docker build $COMMANDS_BUILD
 
 COMMANDS_RUN="\
 	--name $NAME_PROJECT \
+	--rm \
+	-it \
 	$NAME_PROJECT"
+docker run $COMMANDS_RUN
