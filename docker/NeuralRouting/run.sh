4f5bbadb15eeeb2a4c928abe943f8c65a5f61812
PilJoong, in CLO Office <43630935+piljoong-jeong@users.noreply.github.com>
2022-08-13 15:25:58 +0900

    Update run.sh - mounted NeuralRouting project directory
    Todo: it would be great if we share NeuralRouting workspace directly

--- docker/NeuralRouting/run.sh
+++ docker/NeuralRouting/run.sh
@@ -7,8 +7,8 @@
 
 CMD_RUN="-it \
 	--gpus all \
-	--name neuralrouting_ransac \
 	--mount type=bind,src=/mnt/d/RIO10,dst=/opt/dataset \
+	--mount type=bind,src=/root/Documents/Projects/NeuralRouting,dst=/opt/relocalizer_codes/NeuralRouting \
         piljoong/neuralrouting:ransac"
 
 docker run $CMD_RUN
