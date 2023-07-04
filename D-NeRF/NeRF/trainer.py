0945ba601f7aff9a3af715ae32fc8eed9bef995e
PilJoong, in CLO Office <43630935+piljoong-jeong@users.noreply.github.com>
2023-07-04 22:43:58 +0900

    implemented: test rendering

--- D-NeRF/NeRF/trainer.py
+++ D-NeRF/NeRF/trainer.py
@@ -1,3 +1,4 @@
+import imageio.v2 as imageio
 import os
 
 import numpy as np
@@ -6,6 +7,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import NeRF.dataloader
+import NeRF.model
+import NeRF.ops
+import NeRF.rendering
+
 
 def __save_args_and_config(basedir, expname, args):
 
@@ -22,9 +27,46 @@ def __save_args_and_config(basedir, expname, args):
 
     return
 
-def __render_only(
+@torch.no_grad
+def __test_render(
         
-)
+        basedir, 
+        expname, 
+        start,
+
+        render_poses, 
+        hwf, 
+        K, 
+
+        chunk, 
+        render_factor,
+        is_render_test,
+
+        gt_images, 
+
+        **render_test_kwargs
+):
+    """
+    Writes a rendered video for testing purpose
+    """
+
+    test_save_dir = os.path.join(basedir, expname, f"render_only_{'test' if is_render_test else 'path'}_{start:06d}")
+    os.makedirs(test_save_dir, exist_ok=True)
+
+    rgbs, _ = NeRF.rendering.render_to_path(
+        render_poses, 
+        hwf, 
+        K, 
+        chunk, 
+        render_test_kwargs, 
+        gt_imgs=gt_images,
+        savedir=test_save_dir, 
+        render_factor=render_factor
+    )
+    imageio.mimwrite(os.path.join(test_save_dir, "video.mp4"), NeRF.ops.to_8b(rgbs), fps=30, quality=10)
+    print(f"[DEBUG] Done test rendering at {test_save_dir}")
+
+    return
 
 
 def train(args):
@@ -47,6 +89,9 @@ def train(args):
             images = images[..., :3]
     # ---------------------------------------------
 
+    if args.render_test:
+        render_poses = np.array(poses[i_test])
+
     H, W, focal = hwf
     H, W = int(H), int(W)
     hwf = [H, W, focal]
@@ -62,12 +107,32 @@ def train(args):
     __save_args_and_config(basedir, expname, args)
 
 
+    render_train_kwargs, render_test_kwargs, start, grad_vars, optimizer = NeRF.model.create_NeRF(args)
+    global_step = start
+    render_test_kwargs.update(
+        (dict_bounds := {
+            "near": near, 
+            "far": far, 
+        })
+    )
 
-
-    
-    if args.render_test:
-        render_poses = torch.Tensor(np.array(poses[i_test])).to(device="cuda") # TODO: fix this
+    _device = "cuda" # TODO: fix
+    render_poses = torch.Tensor(render_poses).to(_device)
 
     if args.render_only:
-        
+        with torch.no_grad():
+            gt_images = images[i_test] if args.render_test else None
+        return __test_render(
+            basedir, 
+            expname, 
+            start, 
+            render_poses, 
+            hwf, 
+            K, 
+            args.chunk,
+            args.render_factor, 
+            args.render_test, 
+            gt_images=gt_images, 
+            render_test_kwargs=render_test_kwargs
+        )
     
