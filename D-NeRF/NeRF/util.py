4f2abb271a4648d6ffa3f97421cd88a7197d5e6d
PilJoong, in CLO Office <43630935+piljoong-jeong@users.noreply.github.com>
2023-07-19 17:55:29 +0900

    implemented: `get_gpu_memory_usage()`

---
+++ D-NeRF/NeRF/util.py
@@ -0,0 +1,26 @@
+import torch
+
+def humanbytes(B):
+    """Return the given bytes as a human friendly KB, MB, GB, or TB string."""
+    B = float(B)
+    KB = float(1024)
+    MB = float(KB ** 2) # 1,048,576
+    GB = float(KB ** 3) # 1,073,741,824
+    TB = float(KB ** 4) # 1,099,511,627,776
+
+    if B < KB:
+        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+    elif KB <= B < MB:
+        return '{0:.2f} KB'.format(B / KB)
+    elif MB <= B < GB:
+        return '{0:.2f} MB'.format(B / MB)
+    elif GB <= B < TB:
+        return '{0:.2f} GB'.format(B / GB)
+    elif TB <= B:
+        return '{0:.2f} TB'.format(B / TB)
+    
+def get_gpu_memory_usage():
+    tup_memory_category = ("Free", "Total")
+    return tuple(
+        f"{category}: {humanbytes(mb)}" for category, mb 
+        in zip(tup_memory_category, torch.cuda.mem_get_info()))
